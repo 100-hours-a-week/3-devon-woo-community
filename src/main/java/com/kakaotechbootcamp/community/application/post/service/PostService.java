b4ce71053f2b752c3ad7548b15581e61b76eb13a
SH Woo <imdlsrks.mc@gmail.com>
2025-10-11 15:30:07 +0900

    refactor: PostService 좋아요 로직을 PostLike 엔티티 사용하도록 수정

--- src/main/java/com/kakaotechbootcamp/community/application/post/service/PostService.java
+++ src/main/java/com/kakaotechbootcamp/community/application/post/service/PostService.java
@@ -12,8 +12,10 @@ import com.kakaotechbootcamp.community.domain.member.entity.Member;
 import com.kakaotechbootcamp.community.domain.member.repository.MemberRepository;
 import com.kakaotechbootcamp.community.domain.post.entity.Attachment;
 import com.kakaotechbootcamp.community.domain.post.entity.Post;
+import com.kakaotechbootcamp.community.domain.post.entity.PostLike;
 import com.kakaotechbootcamp.community.domain.post.repository.AttachmentRepository;
 import com.kakaotechbootcamp.community.domain.post.repository.CommentRepository;
+import com.kakaotechbootcamp.community.domain.post.repository.PostLikeRepository;
 import com.kakaotechbootcamp.community.domain.post.repository.PostRepository;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
@@ -32,6 +34,7 @@ public class PostService {
     private final MemberRepository memberRepository;
     private final AttachmentRepository attachmentRepository;
     private final CommentRepository commentRepository;
+    private final PostLikeRepository postLikeRepository;
 
     public PostResponse createPost(PostCreateRequest request, Long authorId) {
         Member member = memberRepository.findById(authorId)
@@ -112,20 +115,39 @@ public class PostService {
         postRepository.deleteById(postId);
     }
 
-    public PostLikeResponse likePost(Long postId) {
+    public PostLikeResponse likePost(Long postId, Long memberId) {
         Post post = postRepository.findById(postId)
                 .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
 
+        // 이미 좋아요를 눌렀는지 확인
+        if (postLikeRepository.existsByPostIdAndMemberId(postId, memberId)) {
+            throw new CustomException(ErrorCode.ALREADY_LIKED);
+        }
+
+        // PostLike 엔티티 생성 및 저장
+        PostLike postLike = PostLike.createWithoutId(postId, memberId);
+        postLikeRepository.save(postLike);
+
+        // 좋아요 수 증가
         post.incrementLikes();
         postRepository.save(post);
 
         return PostLikeResponse.of(postId, post.getLikeCount());
     }
 
-    public PostLikeResponse unlikePost(Long postId) {
+    public PostLikeResponse unlikePost(Long postId, Long memberId) {
         Post post = postRepository.findById(postId)
                 .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
 
+        // 좋아요를 눌렀는지 확인
+        if (!postLikeRepository.existsByPostIdAndMemberId(postId, memberId)) {
+            throw new CustomException(ErrorCode.LIKE_NOT_FOUND);
+        }
+
+        // PostLike 엔티티 삭제
+        postLikeRepository.deleteByPostIdAndMemberId(postId, memberId);
+
+        // 좋아요 수 감소
         post.decrementLikes();
         postRepository.save(post);
 
