d9ab4307b5c8dc498c2be4a1f65dcee55621a362
SH Woo <imdlsrks.mc@gmail.com>
2025-10-11 15:29:57 +0900

    feat: PostLike 엔티티 및 Repository 추가

---
+++ src/main/java/com/kakaotechbootcamp/community/domain/post/entity/PostLike.java
@@ -0,0 +1,33 @@
+package com.kakaotechbootcamp.community.domain.post.entity;
+
+import com.kakaotechbootcamp.community.domain.common.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostLike extends BaseEntity {
+
+    private Long id;
+
+    private Long postId;
+
+    private Long memberId;
+
+    public PostLike withId(Long id) {
+        return this.toBuilder().id(id).build();
+    }
+
+    public static PostLike createWithoutId(Long postId, Long memberId) {
+        return PostLike.builder()
+                .postId(postId)
+                .memberId(memberId)
+                .build();
+    }
+}
---
+++ src/main/java/com/kakaotechbootcamp/community/domain/post/repository/PostLikeRepository.java
@@ -0,0 +1,17 @@
+package com.kakaotechbootcamp.community.domain.post.repository;
+
+import com.kakaotechbootcamp.community.domain.post.entity.PostLike;
+import com.kakaotechbootcamp.community.infra.repository.CustomJpaRepository;
+
+import java.util.Optional;
+
+public interface PostLikeRepository extends CustomJpaRepository<PostLike, Long> {
+
+    Optional<PostLike> findByPostIdAndMemberId(Long postId, Long memberId);
+
+    boolean existsByPostIdAndMemberId(Long postId, Long memberId);
+
+    void deleteByPostIdAndMemberId(Long postId, Long memberId);
+
+    long countByPostId(Long postId);
+}
