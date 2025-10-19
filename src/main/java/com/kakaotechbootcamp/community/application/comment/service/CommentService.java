6169f8e278d5c5fe867beaeda329de34800ce302
SH Woo <imdlsrks.mc@gmail.com>
2025-10-19 13:39:11 +0900

    refactory: CQRS 포기 -> 단일 Service로 병합
    - 아직 분리하기에는 이른 부분이 있음
    - 좀 더 복잡해지고 분리해야하는 명확한 이유가 생기면 분리할 것

--- src/main/java/com/kakaotechbootcamp/community/application/comment/service/CommentService.java
+++ src/main/java/com/kakaotechbootcamp/community/application/comment/service/CommentService.java
@@ -1,7 +1,10 @@
 package com.kakaotechbootcamp.community.application.comment.service;
 
+import com.kakaotechbootcamp.community.application.comment.dto.request.CommentCreateRequest;
+import com.kakaotechbootcamp.community.application.comment.dto.request.CommentUpdateRequest;
 import com.kakaotechbootcamp.community.application.comment.dto.response.CommentListResponse;
 import com.kakaotechbootcamp.community.application.comment.dto.response.CommentResponse;
+import com.kakaotechbootcamp.community.application.validator.AccessPolicyValidator;
 import com.kakaotechbootcamp.community.common.exception.CustomException;
 import com.kakaotechbootcamp.community.common.exception.ErrorCode;
 import com.kakaotechbootcamp.community.domain.member.entity.Member;
@@ -20,23 +23,51 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-public class CommentQueryService {
+public class CommentService {
+
     private final CommentRepository commentRepository;
     private final MemberRepository memberRepository;
     private final PostRepository postRepository;
+    private final AccessPolicyValidator accessPolicyValidator;
+
+    public CommentResponse createComment(Long postId, CommentCreateRequest request, Long authorId) {
+        findPostById(postId);
+        Member author = findMemberById(authorId);
+
+        Comment comment = Comment.create(postId, authorId, request.content());
+        commentRepository.save(comment);
+
+        return CommentResponse.of(comment, author);
+    }
+
+    public CommentResponse updateComment(Long commentId, CommentUpdateRequest request, Long requesterId) {
+        Comment comment = findCommentById(commentId);
+        Member author = findMemberById(comment.getAuthorId());
+
+        accessPolicyValidator.checkAccess(comment.getAuthorId(), requesterId);
+
+        comment.updateContent(request.content());
+        commentRepository.save(comment);
+
+        return CommentResponse.of(comment, author);
+    }
+
+    public void deleteComment(Long commentId, Long requesterId) {
+        Comment comment = findCommentById(commentId);
+        accessPolicyValidator.checkAccess(comment.getAuthorId(), requesterId);
+
+        commentRepository.deleteById(comment.getId());
+    }
 
     public CommentResponse getComment(Long commentId) {
-        Comment comment = commentRepository.findById(commentId)
-                .orElseThrow(() -> new CustomException(ErrorCode.COMMENT_NOT_FOUND));
-        Member member = memberRepository.findById(comment.getAuthorId())
-                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+        Comment comment = findCommentById(commentId);
+        Member member = findMemberById(comment.getAuthorId());
 
         return CommentResponse.of(comment, member);
     }
 
     public CommentListResponse getCommentsByPostId(Long postId, int page, int size) {
-        Post post = postRepository.findById(postId)
-                .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
+        findPostById(postId);
 
         List<Comment> comments = commentRepository.findByPostId(postId);
 
@@ -59,5 +90,20 @@ public class CommentQueryService {
                 .toList();
 
         return CommentListResponse.of(postId, commentResponses, page, size);
+    }
+
+    private Post findPostById(Long postId) {
+        return postRepository.findById(postId)
+                .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
+    }
+
+    private Comment findCommentById(Long commentId) {
+        return commentRepository.findById(commentId)
+                .orElseThrow(() -> new CustomException(ErrorCode.COMMENT_NOT_FOUND));
+    }
+
+    private Member findMemberById(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
     }
 }
