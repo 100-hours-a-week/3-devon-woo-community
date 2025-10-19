15381c792a33e2eda786e335e19ebc1332c667fa
SH Woo <imdlsrks.mc@gmail.com>
2025-10-19 12:47:24 +0900

    refactory: SRP - 좋아요 로직을 PostLikeCommandService로 분리
    - PostCommand는 오직 Post에만 집중하고
    - PostLikeCommandService는 Like에만 집중한다.

--- src/main/java/com/kakaotechbootcamp/community/application/post/service/PostCommandService.java
+++ src/main/java/com/kakaotechbootcamp/community/application/post/service/PostCommandService.java
@@ -2,7 +2,6 @@ package com.kakaotechbootcamp.community.application.post.service;
 
 import com.kakaotechbootcamp.community.application.post.dto.request.PostCreateRequest;
 import com.kakaotechbootcamp.community.application.post.dto.request.PostUpdateRequest;
-import com.kakaotechbootcamp.community.application.post.dto.response.PostLikeResponse;
 import com.kakaotechbootcamp.community.application.post.dto.response.PostResponse;
 import com.kakaotechbootcamp.community.common.exception.CustomException;
 import com.kakaotechbootcamp.community.common.exception.ErrorCode;
@@ -11,9 +10,7 @@ import com.kakaotechbootcamp.community.domain.member.entity.Member;
 import com.kakaotechbootcamp.community.domain.member.repository.MemberRepository;
 import com.kakaotechbootcamp.community.domain.post.entity.Attachment;
 import com.kakaotechbootcamp.community.domain.post.entity.Post;
-import com.kakaotechbootcamp.community.domain.post.entity.PostLike;
 import com.kakaotechbootcamp.community.domain.post.repository.AttachmentRepository;
-import com.kakaotechbootcamp.community.domain.post.repository.PostLikeRepository;
 import com.kakaotechbootcamp.community.domain.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -26,7 +23,6 @@ public class PostCommandService {
     private final PostRepository postRepository;
     private final MemberRepository memberRepository;
     private final AttachmentRepository attachmentRepository;
-    private final PostLikeRepository postLikeRepository;
     private final AccessPolicyValidator accessPolicyValidator;
 
     public PostResponse createPost(PostCreateRequest request, Long authorId) {
@@ -65,38 +61,5 @@ public class PostCommandService {
         accessPolicyValidator.checkAccess(post.getAuthorId(), authorId);
 
         postRepository.deleteById(postId);
-    }
-
-    public PostLikeResponse likePost(Long postId, Long memberId) {
-        Post post = postRepository.findById(postId)
-                .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
-
-        if (postLikeRepository.existsByPostIdAndMemberId(postId, memberId)) {
-            throw new CustomException(ErrorCode.ALREADY_LIKED);
-        }
-
-        PostLike postLike = PostLike.create(postId, memberId);
-        postLikeRepository.save(postLike);
-
-        post.incrementLikes();
-        postRepository.save(post);
-
-        return PostLikeResponse.of(postId, post.getLikeCount());
-    }
-
-    public PostLikeResponse unlikePost(Long postId, Long memberId) {
-        Post post = postRepository.findById(postId)
-                .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
-
-        if (!postLikeRepository.existsByPostIdAndMemberId(postId, memberId)) {
-            throw new CustomException(ErrorCode.LIKE_NOT_FOUND);
-        }
-
-        postLikeRepository.deleteByPostIdAndMemberId(postId, memberId);
-
-        post.decrementLikes();
-        postRepository.save(post);
-
-        return PostLikeResponse.of(postId, post.getLikeCount());
     }
 }
