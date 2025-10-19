b4bbb4d6aaa8a44b9b4f042c9a5bcd7a6fa5b25a
SH Woo <imdlsrks.mc@gmail.com>
2025-10-19 12:11:29 +0900

    remove: 미사용 request 제거

--- src/main/java/com/kakaotechbootcamp/community/application/member/dto/request/MemberDeleteRequest.java
+++
@@ -1,8 +0,0 @@
-package com.kakaotechbootcamp.community.application.member.dto.request;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record MemberDeleteRequest(
-        @NotBlank(message = "missing_password")
-        String password
-) {}
