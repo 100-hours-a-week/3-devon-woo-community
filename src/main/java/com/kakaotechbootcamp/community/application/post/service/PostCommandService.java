2712df5c7953d7f78f3ace80b376139cae09b415
SH Woo <imdlsrks.mc@gmail.com>
2025-10-19 12:35:23 +0900

    refactory: SRP - 검증 공통 로직 재사용

--- src/main/java/com/kakaotechbootcamp/community/application/post/service/PostCommandService.java
+++ src/main/java/com/kakaotechbootcamp/community/application/post/service/PostCommandService.java
@@ -6,6 +6,7 @@ import com.kakaotechbootcamp.community.application.post.dto.response.PostLikeRes
 import com.kakaotechbootcamp.community.application.post.dto.response.PostResponse;
 import com.kakaotechbootcamp.community.common.exception.CustomException;
 import com.kakaotechbootcamp.community.common.exception.ErrorCode;
+import com.kakaotechbootcamp.community.application.validator.AccessPolicyValidator;
 import com.kakaotechbootcamp.community.domain.member.entity.Member;
 import com.kakaotechbootcamp.community.domain.member.repository.MemberRepository;
 import com.kakaotechbootcamp.community.domain.post.entity.Attachment;
@@ -26,6 +27,7 @@ public class PostCommandService {
     private final MemberRepository memberRepository;
     private final AttachmentRepository attachmentRepository;
     private final PostLikeRepository postLikeRepository;
+    private final AccessPolicyValidator accessPolicyValidator;
 
     public PostResponse createPost(PostCreateRequest request, Long authorId) {
         Member member = memberRepository.findById(authorId)
@@ -44,7 +46,7 @@ public class PostCommandService {
                 .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
         Member member = memberRepository.findById(post.getAuthorId())
                 .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
-        validateAuthor(post, authorId);
+        accessPolicyValidator.checkAccess(post.getAuthorId(), authorId);
 
         post.updatePost(request.title(), request.content());
         Post savedPost = postRepository.save(post);
@@ -60,7 +62,7 @@ public class PostCommandService {
         Post post = postRepository.findById(postId)
                 .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
 
-        validateAuthor(post, authorId);
+        accessPolicyValidator.checkAccess(post.getAuthorId(), authorId);
 
         postRepository.deleteById(postId);
     }
@@ -96,11 +98,5 @@ public class PostCommandService {
         postRepository.save(post);
 
         return PostLikeResponse.of(postId, post.getLikeCount());
-    }
-
-    private void validateAuthor(Post post, Long authorId) {
-        if (!post.getAuthorId().equals(authorId)) {
-            throw new CustomException(ErrorCode.NO_PERMISSION);
-        }
     }
 }
