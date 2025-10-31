20ffa0f4086e620fb598cd1d774e49d3119968f3
SH Woo <imdlsrks.mc@gmail.com>
2025-10-30 21:13:55 -0700

    feat: ErrorCode 추가
    Post및 Comment의 에러코드 추가

--- src/main/java/com/kakaotechbootcamp/community/common/exception/code/PostErrorCode.java
+++ src/main/java/com/kakaotechbootcamp/community/common/exception/code/PostErrorCode.java
@@ -17,7 +17,18 @@ public enum PostErrorCode implements ErrorCode {
 
     // 좋아요 관련 에러
     ALREADY_LIKED(HttpStatus.CONFLICT, "이미 좋아요를 눌렀습니다"),
-    LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, "좋아요를 찾을 수 없습니다");
+    LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, "좋아요를 찾을 수 없습니다"),
+
+    // 게시글 생성/수정 에러 (400)
+    POST_AUTHOR_REQUIRED(HttpStatus.BAD_REQUEST, "작성자는 필수입니다"),
+    POST_TITLE_REQUIRED(HttpStatus.BAD_REQUEST, "제목은 필수입니다"),
+    POST_TITLE_TOO_LONG(HttpStatus.BAD_REQUEST, "제목은 200자를 초과할 수 없습니다"),
+    POST_CONTENT_REQUIRED(HttpStatus.BAD_REQUEST, "내용은 필수입니다"),
+
+    // 첨부파일 에러 (400)
+    ATTACHMENT_POST_REQUIRED(HttpStatus.BAD_REQUEST, "게시글은 필수입니다"),
+    ATTACHMENT_URL_REQUIRED(HttpStatus.BAD_REQUEST, "첨부파일 URL은 필수입니다"),
+    ATTACHMENT_URL_TOO_LONG(HttpStatus.BAD_REQUEST, "첨부파일 URL은 500자를 초과할 수 없습니다");
 
     private final HttpStatus httpStatus;
     private final String message;
