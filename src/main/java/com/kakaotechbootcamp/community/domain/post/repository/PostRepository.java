219683d37a8d55c0a3284f5a5c934046ebba8de7
SH Woo <imdlsrks.mc@gmail.com>
2025-10-31 14:45:46 -0700

    feat: Fetch Join 방식과 EntityGraph 방식 메서드 제공

--- src/main/java/com/kakaotechbootcamp/community/domain/post/repository/PostRepository.java
+++ src/main/java/com/kakaotechbootcamp/community/domain/post/repository/PostRepository.java
@@ -1,7 +1,20 @@
 package com.kakaotechbootcamp.community.domain.post.repository;
 
 import com.kakaotechbootcamp.community.domain.post.entity.Post;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    // Fetch Join 사용
+    @Query("SELECT p FROM Post p JOIN FETCH p.author")
+    List<Post> findAllWithAuthorByFetchJoin();
+
+    // EntityGraph 사용
+    @EntityGraph(attributePaths = {"author"})
+    @Query("SELECT p FROM Post p")
+    List<Post> findAllWithAuthorByEntityGraph();
 }
