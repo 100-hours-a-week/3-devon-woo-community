2bb9b94645752c69c8df895afc0587ff8a0cd2ab
SH Woo <imdlsrks.mc@gmail.com>
2025-10-30 21:03:42 -0700

    fix: PostLike 엔티티 검증 규칙 변경
    - create시 검증 로직 작성
    - 도메인(엔티티)내에서 검증되는 예외는 커스텀이 아닌 일반 예외로 (보통 서비스에서는 일반예외로 처리)

--- src/main/java/com/kakaotechbootcamp/community/domain/post/entity/PostLike.java
+++ src/main/java/com/kakaotechbootcamp/community/domain/post/entity/PostLike.java
@@ -2,33 +2,43 @@ package com.kakaotechbootcamp.community.domain.post.entity;
 
 import com.kakaotechbootcamp.community.domain.common.BaseEntity;
 import com.kakaotechbootcamp.community.domain.member.entity.Member;
+import com.kakaotechbootcamp.community.domain.post.entity.id.PostLikeId;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.util.Assert;
 
 @Entity
 @Getter
-@Builder(toBuilder = true)
+@Builder(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "post_like")
 public class PostLike extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @EmbeddedId
+    private PostLikeId id;
 
-    @ManyToOne
-    @JoinColumn(name = "post_id")
+    @MapsId("postId")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
     private Post post;
 
-    @ManyToOne
-    @JoinColumn(name = "member_id")
+    @MapsId("memberId")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
     private Member member;
 
     public static PostLike create(Post post, Member member) {
+        validateCreate(post, member);
         return PostLike.builder()
+                .id(PostLikeId.create(post.getId(), member.getId()))
                 .post(post)
                 .member(member)
                 .build();
+    }
+
+    private static void validateCreate(Post post, Member member) {
+        Assert.notNull(post, "post required");
+        Assert.notNull(member, "member required");
     }
 }
